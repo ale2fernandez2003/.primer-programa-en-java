2c9c0acbc8537fa5a7ed0890ab3300e936fbfa00
Alejandro <ale2fernandez2003@gmail.com>
2022-10-10 09:02:34 +0200

    Puse la mi comida favorita

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
 	public static void main(String args[]){
 		System.out.println("Fernandez Fernandez");
 		System.out.println("Tendo 19 años y naci en el 2003");
+		System.out.println("Mi comida favorita es la tortilla española");
 	}
 }
