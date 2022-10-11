6f4192e41e511f6cff7e074c6f738716519fabcf
Alejandro <ale2fernandez2003@gmail.com>
2022-10-11 09:09:18 +0200

    quite mis apellidos y puse mi grupo de musica favorito

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,8 @@
 public class Prueba{
 	public static void main(String args[]){
-		System.out.println("Fernandez Fernandez");
+
 		System.out.println("Tendo 19 años y naci en el 2003");
 		System.out.println("Mi comida favorita es la tortilla española");
+		System.out.println("Mi grupo favorito de musica es ACDC");
 	}
 }
